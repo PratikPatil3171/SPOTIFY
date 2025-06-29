439c6a25997597236979793ebd295513e504328e
Pratik-Patil-P <pratikjp.290606@gmail.com>
2025-06-29 16:09:28 +0530

    First Step

---
+++ index.js
@@ -0,0 +1,23 @@
+// const searchInput = document.getElementById("search");
+// const clearBtn = document.getElementById("clearBtn");
+
+// clearBtn.addEventListener("click", () => {
+//   searchInput.value = "";
+//   searchInput.focus();
+// });
+document.body.addEventListener('mouseover', e => {
+  let button = e.target.closest('button');
+  if (!button) { return; }
+  button.style.fontSize = '1.15rem';
+});
+
+
+document.body.addEventListener('mouseout', e => {
+  let button = e.target.closest('button');
+  if (!button) { return; }
+  button.style.fontSize= '1.1rem';
+});
+document.querySelector(".home").addEventListener("mouseover",()=>{
+  
+})
+
