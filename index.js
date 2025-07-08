47822b0a48be2c3e71851cc987bae4ed17eec2c4
Pratik-Patil-P <pratikjp.290606@gmail.com>
2025-07-08 10:21:34 +0530

    Scroll bar

--- index.js
+++ index.js
@@ -5,19 +5,19 @@
 //   searchInput.value = "";
 //   searchInput.focus();
 // });
-document.body.addEventListener('mouseover', e => {
-  let button = e.target.closest('button');
-  if (!button) { return; }
-  button.style.fontSize = '1.15rem';
-});
+// document.body.addEventListener('mouseover', e => {
+//   let button = e.target.closest('button');
+//   if (!button) { return; }
+//   button.style.fontSize = '1.15rem';
+// });
 
 
-document.body.addEventListener('mouseout', e => {
-  let button = e.target.closest('button');
-  if (!button) { return; }
-  button.style.fontSize= '1.1rem';
-});
-document.querySelector(".home").addEventListener("mouseover",()=>{
+// document.body.addEventListener('mouseout', e => {
+//   let button = e.target.closest('button');
+//   if (!button) { return; }
+//   button.style.fontSize= '1.1rem';
+// });
+// document.querySelector(".home").addEventListener("mouseover",()=>{
   
-})
+// })
 
